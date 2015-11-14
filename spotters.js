f2d50b40e8cd3b7bf710aa653f4df101febf3c7f
I <alexis.medvedeff@sabf.org.ar>
2015-11-14 20:36:15 -0300

    Update spotters map with different markers and layers

--- spotters.js
+++ spotters.js
@@ -1,17 +1,38 @@
 /* Define the map from mapbox */
 var map = L.map('map').setView([0, -28], 3);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxlbWVkdmUiLCJhIjoiY2llcHNoeHByMDAxbndqa21zdjdwYmJkciJ9.Ch1gM-PFeR7YhhKpqV4I7Q', {
+var satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxlbWVkdmUiLCJhIjoiY2llcHNoeHByMDAxbndqa21zdjdwYmJkciJ9.Ch1gM-PFeR7YhhKpqV4I7Q', {
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  id: 'alemedve.nlc9d0ic',
+  id: 'mapbox.satellite',
   accesToken: 'pk.eyJ1IjoiYWxlbWVkdmUiLCJhIjoiY2llcHNoeHByMDAxbndqa21zdjdwYmJkciJ9.Ch1gM-PFeR7YhhKpqV4I7Q'
 }).addTo(map);
+
+var streets   = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxlbWVkdmUiLCJhIjoiY2llcHNoeHByMDAxbndqa21zdjdwYmJkciJ9.Ch1gM-PFeR7YhhKpqV4I7Q', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.streets',
+  accesToken: 'pk.eyJ1IjoiYWxlbWVkdmUiLCJhIjoiY2llcHNoeHByMDAxbndqa21zdjdwYmJkciJ9.Ch1gM-PFeR7YhhKpqV4I7Q'
+}).addTo(map);
+
+var baseMaps = {
+    "Satellite": satellite,
+    "Streets": streets
+};
+
+//L.control.layers(baseMaps).addTo(map);
 var pointIcon = L.icon({
-  iconUrl: 'point.png',
+  iconUrl: 'redpoint.png',
   iconSize: [10, 10]
-})
+});
+
+var bluemarker = L.icon({
+  iconUrl: 'bluemarker.png',
+  iconSize: [12, 20]
+});
 
 /* Get json from Flickr */
 function get_json(url, location, callback) {
@@ -70,21 +91,33 @@ function onXClick(e) {
 document.getElementById("photofeedclose").addEventListener("click", onXClick);
 
 /* Retrieve airport data and parse */
+
 var results = Papa.parse("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat", {
   download: true,
   complete: function(results) {
     console.log(results);
+    var emptyAirports = L.markerClusterGroup();
+    var photoAirports = L.markerClusterGroup();
+    var overlayMaps = {
+      "Empty": emptyAirports,
+      "With photos": photoAirports
+    };
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
     // Limited to 100 results for testing purposes
     // i < results.data.length
-    for (i=0; i<100; i++) {
+    for (i=0; i<results.data.length; i++) {
       get_json("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b8755d103368823ee3ca52487500e945&tags=plane&lat=" + results.data[i][6] + "&lon=" + results.data[i][7] + "&radius=5&format=json&nojsoncallback=1",
       [results.data[i][6], results.data[i][7]],
         function (resp, location) {
+          resp.lat = location[0];
+          resp.lon = location[1];
+          var icon = pointIcon;
+          var airports = emptyAirports;
           if(resp.photos.total != "0"){
-              resp.lat = location[0];
-              resp.lon = location[1];
-              L.marker([location[0], location[1]], {icon: pointIcon, title: resp.photos.total}).addTo(map).on('click', onPointClick, resp);;
+              icon = bluemarker;
+              airports = photoAirports;
           }
+          airports.addLayer(L.marker([location[0], location[1]], {icon: icon, title: resp.photos.total}).on('click', onPointClick, resp));
         });
     }
   }
